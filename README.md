bfcbc16d3edad5c8163fd88a10f75f79406eb2a5
Sven Gaubert <svengau@gmail.com>
2020-05-26 10:41:14 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,11 +1,110 @@
 # Gridsome Source for Printful
 
-Source plugin for fetching data from [Prinful](https://www.printful.com/) into [Gridsome](https://gridsome.org/).
+Source plugin for fetching data from [Printful](https://www.printful.com/) into [Gridsome](https://gridsome.org/).
 
 ## Install
 
-`npm install --save gridsome-source-stripe`
+`npm install --save gridsome-source-printful`
 
 or
 
-`yarn add gridsome-source-stripe`
+`yarn add gridsome-source-printful`
+
+## How to use
+
+```javascript
+// In your gridsome.config.js
+plugins: [
+  {
+    use: 'gridsome-source-printful',
+    options: {
+      objectTypes: ['SyncProduct', 'WarehouseProduct', 'Country', 'TaxRate'],
+      apiKey: 'xxxxxxxxx',
+      // optional params
+      downloadFiles: true,
+      imageDirectory: 'printful_images',
+    },
+  },
+];
+```
+
+if downloadFiles is `true` the plugin will download all assets associated to fields `image` and `images` into `imageDirectory`.
+
+## How to query and filter
+
+For example, you can query the product nodes created from Printful with the following:
+
+```javascript
+query {
+  allPrintfulProduct {
+    edges {
+      node {
+        id
+        external_id
+        name
+        slug
+        thumbnail_url
+        thumbnail_img (width: 100 height: 100)
+        __typename
+        variants {
+          id
+          variant_id
+          external_id
+          sync_product_id
+          retail_price
+          currency
+          name
+          sku
+          files {
+            id
+            filename
+            thumbnail_url
+          }
+        }
+      }
+    }
+  }
+}
+
+```
+
+and you can filter specific node using this:
+
+```javascript
+query product($id: String!) {
+  printfulProduct(id: $id) {
+    id
+    external_id
+    name
+  }
+}
+```
+
+Another example with the countries:
+
+```javascript
+query {
+  allPrintfulCountry {
+    totalCount
+    edges {
+      node {
+        id
+        name
+        code
+        states {code name}
+      }
+    }
+  }
+}
+```
+
+## Documentation
+
+The official Printful API documentation is [here](https://www.printful.com/docs/products)
+
+## Thanks
+
+This plugin has been:
+
+- largely inspired by the great [Gatsby-source-printful](https://github.com/ynnoj/gatsby-source-printful) developed by [Jonathan Steele](https://twitter.com/ynnoj)
+- developed with the kind support of [Fullstack Rocket](https://www.fullstackrocket.com/)
---
+++ index.js
@@ -0,0 +1,287 @@
+const slugify = require('slugify');
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const https = require('https');
+const debug = require('debug')('gridsome-source-printful');
+
+const createDirectory = dir => {
+  const pwd = path.join(process.cwd(), dir);
+
+  if (!fs.existsSync(pwd)) {
+    fs.mkdirSync(pwd);
+  }
+
+  return pwd;
+};
+
+class PrintfulSource {
+  static defaultOptions() {
+    return {
+      typeName: 'Printful',
+      objectTypes: ['SyncProduct', 'WarehouseProduct', 'Country', 'TaxRate'],
+      secretKey: null,
+      paginationLimit: 20,
+      downloadFiles: false,
+      imageDirectory: 'printful_images',
+    };
+  }
+
+  constructor(api, options) {
+    this.options = options;
+    api.loadSource(args => this.fetchContent(args));
+  }
+
+  getPrintfulClient() {
+    const { apiKey } = this.options;
+
+    const client = axios.create({
+      baseURL: 'https://api.printful.com',
+      headers: {
+        Authorization: `Basic ${Buffer.from(apiKey).toString('base64')}`,
+      },
+    });
+    client.interceptors.request.use(function(config) {
+      debug(`${config.method.toUpperCase()} ${config.url}`);
+      return config;
+    });
+
+    return client;
+  }
+
+  /**
+   * Download the images locally
+   *
+   * @param {*} pwd
+   * @param {*} prefix
+   * @param {*} imageUrl
+   */
+  async downloadImage(pwd, prefix, imageUrl) {
+    if (!imageUrl) {
+      return null;
+    }
+    const filename = `${prefix}_${imageUrl
+      .split('/')
+      .pop()
+      .toLowerCase()}`;
+    const filePath = path.resolve(pwd, filename);
+
+    if (fs.existsSync(filePath)) {
+      debug(`Image ${filename} already downloaded `);
+      return filePath;
+    }
+
+    return new Promise((resolve, reject) => {
+      debug(`Downloading ${imageUrl}`);
+      const file = fs.createWriteStream(filePath);
+
+      https
+        .get(imageUrl, response => {
+          response.pipe(file);
+          file.on('finish', () => {
+            console.info('Download finished!');
+            file.close(() => resolve(filePath));
+          });
+        })
+        .on('error', err => {
+          console.error(`Error on processing image ${filename}`);
+          console.error(err.message);
+          fs.unlink(filePath, err => {
+            if (err) {
+              reject(err);
+            }
+
+            debug(`Removed the ${filePath} image correct`);
+            resolve(filePath);
+          });
+        });
+    });
+  }
+
+  async getAllItems({ url }) {
+    const printful = this.getPrintfulClient();
+    const { paginationLimit } = this.options;
+
+    let records = [];
+    let keepGoing = true;
+    let offset = 0;
+
+    while (keepGoing) {
+      const {
+        data: { paging, result },
+      } = await printful.get(
+        `${url}?limit=${paginationLimit}&offset=${offset}`
+      );
+      records = [...records, ...result];
+      offset += paginationLimit;
+
+      if (result.length < paginationLimit || paging.total === records.length) {
+        keepGoing = false;
+
+        return records;
+      }
+    }
+    return records;
+  }
+
+  async fetchSyncProduct({ pwd, addCollection }) {
+    const { downloadFiles, typeName } = this.options;
+    const printful = this.getPrintfulClient();
+
+    const result = await this.getAllItems({ url: 'sync/products' });
+    const products = await Promise.all(
+      result.map(async ({ id }) =>
+        printful
+          .get(`sync/products/${id}`)
+          .then(({ data: { result: { sync_product, sync_variants } } }) => ({
+            ...sync_product,
+            variants: sync_variants.map(o => ({
+              ...o,
+              retail_price: parseFloat(o.retail_price),
+            })),
+          }))
+      )
+    );
+
+    if (downloadFiles) {
+      await Promise.all(
+        products.map(async node => {
+          if (node.thumbnail_url) {
+            node.thumbnail_img = await this.downloadImage(
+              pwd,
+              node.id,
+              node.thumbnail_url
+            );
+          }
+        })
+      );
+    }
+
+    // add slug
+    const contentType = addCollection({ typeName: `${typeName}SyncProduct` });
+    products.map(async node => {
+      if (node.name) {
+        node.slug = slugify(node.name, { lower: true });
+      }
+
+      debug(`Add node SyncProduct ${node.id}`);
+      return contentType.addNode(node);
+    });
+  }
+
+  /**
+   * Retrieve all warehouse products
+   *
+   * @param {*} param0
+   */
+  async fetchWarehouseProduct({ pwd, addCollection }) {
+    debug('fetchWarehouseProducts');
+    const { downloadFiles, typeName } = this.options;
+
+    const products = await this.getAllItems({ url: 'warehouse/products' });
+
+    // if (downloadFiles) {
+    //   await Promise.all(
+    //     products.map(async node => {
+    //       if (node.thumbnail_url) {
+    //         node.thumbnail_img = await this.downloadImage(
+    //           pwd,
+    //           node.id,
+    //           node.thumbnail_url
+    //         );
+    //       }
+    //     })
+    //   );
+    // }
+
+    // add slug
+    const contentType = addCollection({
+      typeName: `${typeName}WarehouseProduct`,
+    });
+    products.map(async node => {
+      if (node.name) {
+        node.slug = slugify(node.name, { lower: true });
+      }
+
+      debug(`Add node WarehouseProduct ${node.id}`);
+      return contentType.addNode(node);
+    });
+  }
+
+  /**
+   * fetch countries
+   * @param {*} param0
+   */
+  async fetchCountry({ addCollection }) {
+    debug('fetchCountries');
+    const { typeName } = this.options;
+    const printful = this.getPrintfulClient();
+
+    const {
+      data: { result: countries },
+    } = await printful.get(`countries`);
+
+    const contentType = addCollection({ typeName: `${typeName}Country` });
+    countries.map(async node => {
+      node.id = node.code;
+      if (node.name) {
+        node.slug = slugify(node.name, { lower: true });
+      }
+      debug(`Add node Country ${node.id}`);
+      return contentType.addNode(node);
+    });
+  }
+
+  /**
+   * fetch tax rates
+   * @param {*} param0
+   */
+  async fetchTaxRate({ addCollection }) {
+    const { typeName } = this.options;
+    const printful = this.getPrintfulClient();
+
+    const {
+      data: { result: countries },
+    } = await printful.get(`tax/countries`);
+
+    const contentType = addCollection({ typeName: `${typeName}TaxRate` });
+    countries.map(async node => {
+      if (node.name) {
+        node.slug = slugify(node.name, { lower: true });
+      }
+
+      debug(`Add node TaxRate ${node.id}`);
+      return contentType.addNode(node);
+    });
+  }
+
+  /**
+   * fetch Content
+   */
+  async fetchContent(store) {
+    const { addCollection } = store;
+    const { downloadFiles, objectTypes } = this.options;
+
+    let pwd;
+    if (downloadFiles) {
+      pwd = createDirectory(this.options.imageDirectory);
+    }
+
+    await Promise.all(
+      objectTypes
+        .map(objectType => `fetch${objectType}`)
+        .filter(method => {
+          !this[method] && console.error(`method ${method} doesn't exists`);
+          return !!this[method];
+        })
+        .map(method =>
+          this[method]({
+            pwd,
+            addCollection,
+          })
+        )
+    );
+  }
+}
+
+module.exports = PrintfulSource;
